417c2aed57f11cfe53eae14f0a90106133b13f51
hosein-home <mohammadkhaleghy@gmail.com>
2025-03-31 13:28:37 +0330

    teriak

---
+++ login/script.js
@@ -0,0 +1,6 @@
+let swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+});
--- login/sign in/sing test/script test.js
+++
@@ -1,8 +0,0 @@
-const check = localStorage.getItem("change text");
-
-if(check == "true"){
-    document.getElementById("h1-tag").innerText = "ورود شما با موفقیت انجام شد";
-    localStorage.setItem("change text", "false")
-}else{
-    document.getElementById("h1-tag").innerText = " ثبت نام شما با موفقیت انجام شد "
-}
